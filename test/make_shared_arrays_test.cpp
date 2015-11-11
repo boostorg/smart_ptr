e13beef5dfd9186f17e9ed63fbd6fd96b48b3eb0
Glen Fernandes <glenjofe@gmail.com>
2015-11-11 01:26:15 -0500

    Fix formatting in headers and tests

--- test/make_shared_arrays_test.cpp
+++ test/make_shared_arrays_test.cpp
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2012-2014 Glen Joseph Fernandes 
- * glenfe at live dot com
- *
- * Distributed under the Boost Software License, 
- * Version 1.0. (See accompanying file LICENSE_1_0.txt 
- * or copy at http://boost.org/LICENSE_1_0.txt)
- */
+(c) 2012-2015 Glen Joseph Fernandes
+<glenjofe -at- gmail.com>
+
+Distributed under the Boost Software
+License, Version 1.0.
+http://boost.org/LICENSE_1_0.txt
+*/
 #include <boost/core/lightweight_test.hpp>
 #include <boost/smart_ptr/make_shared.hpp>
 

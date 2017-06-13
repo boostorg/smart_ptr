27898b0823949b05c4c8c7d3ad5ab68aa8ee2328
Peter Dimov <pdimov@pdimov.com>
2017-06-13 20:57:12 +0300

    Constrain conversions to shared_ptr/weak_ptr

--- include/boost/smart_ptr/local_shared_ptr.hpp
+++ include/boost/smart_ptr/local_shared_ptr.hpp
@@ -474,7 +474,11 @@ public:
 
     // conversions to shared_ptr, weak_ptr
 
+#if !defined( BOOST_SP_NO_SP_CONVERTIBLE ) && !defined(BOOST_NO_CXX11_FUNCTION_TEMPLATE_DEFAULT_ARGS)
+    template<class Y, class E = typename boost::detail::sp_enable_if_convertible<T,Y>::type> operator shared_ptr<Y>() const BOOST_SP_NOEXCEPT
+#else
     template<class Y> operator shared_ptr<Y>() const BOOST_SP_NOEXCEPT
+#endif
     {
         boost::detail::sp_assert_convertible<T, Y>();
 
@@ -488,7 +492,11 @@ public:
         }
     }
 
+#if !defined( BOOST_SP_NO_SP_CONVERTIBLE ) && !defined(BOOST_NO_CXX11_FUNCTION_TEMPLATE_DEFAULT_ARGS)
+    template<class Y, class E = typename boost::detail::sp_enable_if_convertible<T,Y>::type> operator weak_ptr<Y>() const BOOST_SP_NOEXCEPT
+#else
     template<class Y> operator weak_ptr<Y>() const BOOST_SP_NOEXCEPT
+#endif
     {
         boost::detail::sp_assert_convertible<T, Y>();
 

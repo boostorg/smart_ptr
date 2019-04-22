a571b3a25082e383d63725ae3cc057fe432f449c
Peter Dimov <pdimov@gmail.com>
2019-04-22 06:07:36 +0300

    Only include core/typeinfo.hpp when needed

--- include/boost/smart_ptr/detail/sp_typeinfo_.hpp
+++ include/boost/smart_ptr/detail/sp_typeinfo_.hpp
@@ -15,6 +15,10 @@
 //  See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/config.hpp>
+
+#if defined( BOOST_NO_TYPEID ) || defined( BOOST_NO_STD_TYPEINFO )
+
 #include <boost/core/typeinfo.hpp>
 
 namespace boost
@@ -30,5 +34,25 @@ typedef boost::core::typeinfo sp_typeinfo_;
 } // namespace boost
 
 #define BOOST_SP_TYPEID_(T) BOOST_CORE_TYPEID(T)
+
+#else // defined( BOOST_NO_TYPEID ) || defined( BOOST_NO_STD_TYPEINFO )
+
+#include <typeinfo>
+
+namespace boost
+{
+
+namespace detail
+{
+
+typedef std::type_info sp_typeinfo_;
+
+} // namespace detail
+
+} // namespace boost
+
+#define BOOST_SP_TYPEID_(T) typeid(T)
+
+#endif // defined( BOOST_NO_TYPEID ) || defined( BOOST_NO_STD_TYPEINFO )
 
 #endif  // #ifndef BOOST_SMART_PTR_DETAIL_SP_TYPEINFO_HPP_INCLUDED

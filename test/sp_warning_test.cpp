977544feda129535e84c05e91c1576e7fd86f72d
Peter Dimov <pdimov@gmail.com>
2020-04-08 21:25:36 +0300

    Add sp_warning_test

---
+++ test/sp_warning_test.cpp
@@ -0,0 +1,13 @@
+// Copyright 2020 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#if defined(__GNUC__) && __GNUC__ >= 5
+# pragma GCC diagnostic error "-Wsuggest-override"
+#endif
+
+#include <boost/smart_ptr.hpp>
+
+int main()
+{
+}

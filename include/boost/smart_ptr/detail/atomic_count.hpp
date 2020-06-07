7c01e640f7b2a68579e3410cf65d48f2ba655eca
Peter Dimov <pdimov@gmail.com>
2020-06-07 06:05:17 +0300

    Use int_least32_t in atomic_count_sync; prefer it to atomic_count_gcc_x86; mark latter obsolete

--- include/boost/smart_ptr/detail/atomic_count.hpp
+++ include/boost/smart_ptr/detail/atomic_count.hpp
@@ -76,11 +76,11 @@
 #elif !defined( BOOST_NO_CXX11_HDR_ATOMIC )
 # include <boost/smart_ptr/detail/atomic_count_std_atomic.hpp>
 
-#elif defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) ) && !defined( __PATHSCALE__ )
-# include <boost/smart_ptr/detail/atomic_count_gcc_x86.hpp>
-
 #elif defined( BOOST_SP_HAS_SYNC_INTRINSICS )
 # include <boost/smart_ptr/detail/atomic_count_sync.hpp>
+
+#elif defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) ) && !defined( __PATHSCALE__ )
+# include <boost/smart_ptr/detail/atomic_count_gcc_x86.hpp>
 
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
 # include <boost/smart_ptr/detail/atomic_count_win32.hpp>
